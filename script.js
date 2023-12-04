f43ff724093ee1429a5f71dba111513fb0e7568f
Michal Kudyk <kudykmichal@gmail.com>
2023-12-04 02:11:44 +0100

    script change

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-async function getQuestionsFromPDF(pdfPath) {
-  try {
-    const loadingTask = pdfjsLib.getDocument(pdfPath);
-    const pdf = await loadingTask.promise;
-    const questions = [];
+window.addEventListener('DOMContentLoaded', (event) => {
+  async function getQuestionsFromPDF(pdfPath) {
+    try {
+      const loadingTask = pdfjsLib.getDocument(pdfPath);
+      const pdf = await loadingTask.promise;
+      const questions = [];
 
-    const numPages = pdf.numPages;
+      const numPages = pdf.numPages;
 
-    for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
-      const page = await pdf.getPage(pageNumber);
-      const textContent = await page.getTextContent();
-      const pageQuestions = textContent.items
-        .map(item => item.str)
-        .filter(text => text.toLowerCase().startsWith('pytanie nr'));
+      for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
+        const page = await pdf.getPage(pageNumber);
+        const textContent = await page.getTextContent();
+        const pageQuestions = textContent.items
+          .map(item => item.str)
+          .filter(text => text.toLowerCase().startsWith('pytanie nr'));
 
-      questions.push(...pageQuestions);
+        questions.push(...pageQuestions);
+      }
+
+      return questions;
+    } catch (error) {
+      console.error('Błąd podczas pobierania pytań z pliku PDF:', error);
+      return [];
     }
-
-    return questions;
-  } catch (error) {
-    console.error('Błąd podczas pobierania pytań z pliku PDF:', error);
-    return [];
   }
-}
 
-async function displayQuestions() {
-  const pdfPath = './resources/LEK-choroby_wewnętrzne.pdf'; 
-  const questions = await getQuestionsFromPDF(pdfPath);
+  async function displayQuestions() {
+    const pdfPath = './resources/LEK-choroby_wewnętrzne.pdf'; 
+    const questions = await getQuestionsFromPDF(pdfPath);
 
-  const questionsContainer = document.getElementById('questions-container');
+    const questionsContainer = document.getElementById('questions-container');
 
-  questions.forEach((question, index) => {
-    const div = document.createElement('div');
-    div.className = 'question';
-    div.textContent = question;
-    questionsContainer.appendChild(div);
-
-    setTimeout(() => {
-      div.style.display = 'block';
+    questions.forEach((question, index) => {
+      const div = document.createElement('div');
+      div.className = 'question';
+      div.textContent = question;
+      questionsContainer.appendChild(div);
 
       setTimeout(() => {
-        div.style.display = 'none';
-      }, 20000 * (index + 1)); 
-    }, 20000 * index); 
-  });
-}
+        div.style.display = 'block';
 
-displayQuestions();
+        setTimeout(() => {
+          div.style.display = 'none';
+        }, 20000 * (index + 1)); 
+      }, 20000 * index); 
+    });
+  }
+  displayQuestions();
+});
